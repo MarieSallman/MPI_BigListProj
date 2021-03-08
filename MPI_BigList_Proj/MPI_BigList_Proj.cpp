64ed30f8450d0a7256cfcdaba7fd91b9a3502a76
Marie Sallman <Marie.Sallman@trojans.dsu.edu>
2021-03-08 11:44:54 -0600

    Using MPI to create a series of arrays and perform a number of functions.

--- MPI_BigList_Proj/MPI_BigList_Proj.cpp
+++ MPI_BigList_Proj/MPI_BigList_Proj.cpp
@@ -1,20 +1,131 @@
-// MPI_BigList_Proj.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <unistd.h>
+#include <time.h>
 
-#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+#define NUM_ITEMS 1000
+int high1; //for M: 1 to high1
+int high2; //for N: 1 to high2
+
+int array1[NUM_ITEMS];
+int array2[NUM_ITEMS];
+int array3[NUM_ITEMS];
+int array4[NUM_ITEMS];
+int array5[NUM_ITEMS];
+int count = 0; //number of items in the array
+
+void Usage(char* string) {
+    printf("Usage: %s <search file> <pattern file>\n", string);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void diff(int a[], int c[], int count)
+{
+    int i;
+    for (i = 0; i < count; i++)
+    {
+        c[i] = (a[i] / 4 - 3);
+        if (c[i] < 0) {
+            c[i] = 0;
+        }
+    }
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void print(int a[], char s, int count)
+{
+    int i;
+    printf("\n%c:  ", s);
+    for (i = 0; i < count; i++)
+    {
+        printf("%d ", a[i]);
+    }
+}
+void boss(int rank, int size, int count)
+{
+    //scatter the M and N to all the workers
+    int M[count];
+    int P[count];
+    int work[count]; //small list that boss calculated
+    MPI_Scatter(array1, count, MPI_INT, M, count, MPI_INT, 0, MPI_COMM_WORLD);
+
+    //do the work for p array
+    diff(M, work, count);
+
+    //gather the work from everybody
+    MPI_Gather(&work, count, MPI_INT, array3, count, MPI_INT, 0, MPI_COMM_WORLD);
+
+    //print the array out
+    print(array3, 'P', count);
+}
+
+void worker(int rank, int size, int count)
+{
+    //scatter the M and N to all the workers
+    int M[count];
+    int P[count];
+    int work[count]; //small list that boss calculated
+    MPI_Scatter(array1, count, MPI_INT, M, count, MPI_INT, 0, MPI_COMM_WORLD);
+
+    //do the work for p array
+    diff(M, work, count);
+    //gather the work from everybody
+    MPI_Gather(&work, count, MPI_INT, array3, count, MPI_INT, 0, MPI_COMM_WORLD);
+
+}
+
+
+int main(int argc, char* argv[])
+{
+    // Initialize the MPI environment
+    MPI_Init(&argc, &argv);
+    double start_time = MPI_Wtime();
+    // Find out rank, size
+    int rank;
+    int wsize;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &wsize);
+
+    if (argc < 1 || argc > 2) {
+        Usage(argv[0]);
+        return 0;
+    }
+
+    if (argc == 2) {
+        FILE* file;
+
+        file = fopen(argv[1], "r");
+
+        if (!file) {
+            printf("Problem with opening file\n");
+            return 0;
+        }
+
+        while (count < NUM_ITEMS && fscanf(file, "%d", &array1[count]) == 1)
+            count++;
+
+        fclose(file);
+
+    }
+
+    if (rank == 0)
+    {
+        print(array1, 'M', count);
+        boss(0, wsize, count);
+    }
+    else
+    {
+        worker(rank, wsize, count);
+    }
+
+
+    double end_time = MPI_Wtime();
+    if (rank == 0)
+    {
+        printf("Time: %f\n", end_time - start_time);
+    }
+    MPI_Finalize();
+    return 0;
+}
